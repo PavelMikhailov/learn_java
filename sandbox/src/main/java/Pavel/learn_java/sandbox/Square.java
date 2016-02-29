8c57c7428dcd43039055bb6e197be3402cd961cf
PavelMikhailov <mihailov_pd@mail.ru>
2016-02-29 22:07:26 +0300

    Созданы классы Square  и Rectangle

---
+++ sandbox/src/main/java/Pavel/learn_java/sandbox/Square.java
@@ -0,0 +1,17 @@
+package Pavel.learn_java.sandbox;
+
+/**
+ * Created by Pavel on 29.02.2016.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+}
