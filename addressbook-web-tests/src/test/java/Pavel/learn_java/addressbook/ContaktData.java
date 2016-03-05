9b013709cecbeee632f34f43c06f58b037acec09
PavelMikhailov <mihailov_pd@mail.ru>
2016-03-05 22:53:24 +0300

    Создан тест NewContakt и класс ContaktData

---
+++ addressbook-web-tests/src/test/java/Pavel/learn_java/addressbook/ContaktData.java
@@ -0,0 +1,115 @@
+package Pavel.learn_java.addressbook;
+
+public class ContaktData {
+  private final String name;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String workaddress;
+  private final String homenumber;
+  private final String mobilenumber;
+  private final String worknumber;
+  private final String faxnumber;
+  private final String email1;
+  private final String email2;
+  private final String email3;
+  private final String homepage;
+  private final String homeaddress;
+  private final String housenumber;
+  private final String notes;
+
+  public ContaktData(String name, String middlename, String lastname, String nickname, String title, String company, String workaddress, String homenumber, String mobilenumber, String worknumber, String faxnumber, String email1, String email2, String email3, String homepage, String homeaddress, String housenumber, String notes) {
+    this.name = name;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.workaddress = workaddress;
+    this.homenumber = homenumber;
+    this.mobilenumber = mobilenumber;
+    this.worknumber = worknumber;
+    this.faxnumber = faxnumber;
+    this.email1 = email1;
+    this.email2 = email2;
+    this.email3 = email3;
+    this.homepage = homepage;
+    this.homeaddress = homeaddress;
+    this.housenumber = housenumber;
+    this.notes = notes;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getWorkaddress() {
+    return workaddress;
+  }
+
+  public String getHomenumber() {
+    return homenumber;
+  }
+
+  public String getMobilenumber() {
+    return mobilenumber;
+  }
+
+  public String getWorknumber() {
+    return worknumber;
+  }
+
+  public String getFaxnumber() {
+    return faxnumber;
+  }
+
+  public String getEmail1() {
+    return email1;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+
+  public String getEmail3() {
+    return email3;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public String getHomeaddress() {
+    return homeaddress;
+  }
+
+  public String getHousenumber() {
+    return housenumber;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/Pavel/learn_java/addressbook/NewContaktCreationTest.java
@@ -0,0 +1,122 @@
+package Pavel.learn_java.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class NewContaktCreationTest {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).sendKeys("\\undefined");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void NewContaktCreationTest() {
+        newContaktCreation();
+        fillContaktForm(new ContaktData("Ivan", "II", "Ivanov", "Vanya", "employee", "Company", "address", "4444444", "5555555", "6666666", "7777777", "ivan.ivanovii.@company.com", "ivan@home.ru", "ivan@hame2.ru", "www.ivan.ru", "Address", "Number", "edit"));
+        submitNewContakt();
+    }
+
+    private void submitNewContakt() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContaktForm(ContaktData ContaktData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(ContaktData.getName());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(ContaktData.getMiddlename());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(ContaktData.getLastname());
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(ContaktData.getNickname());
+        wd.findElement(By.name("title")).click();
+        wd.findElement(By.name("title")).clear();
+        wd.findElement(By.name("title")).sendKeys(ContaktData.getTitle());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(ContaktData.getCompany());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(ContaktData.getWorkaddress());
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(ContaktData.getHomenumber());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(ContaktData.getMobilenumber());
+        wd.findElement(By.name("work")).click();
+        wd.findElement(By.name("work")).clear();
+        wd.findElement(By.name("work")).sendKeys(ContaktData.getWorknumber());
+        wd.findElement(By.name("fax")).click();
+        wd.findElement(By.name("fax")).clear();
+        wd.findElement(By.name("fax")).sendKeys(ContaktData.getFaxnumber());
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(ContaktData.getEmail1());
+        wd.findElement(By.name("email2")).click();
+        wd.findElement(By.name("email2")).clear();
+        wd.findElement(By.name("email2")).sendKeys(ContaktData.getEmail2());
+        wd.findElement(By.name("email3")).click();
+        wd.findElement(By.name("email3")).clear();
+        wd.findElement(By.name("email3")).sendKeys(ContaktData.getEmail3());
+        wd.findElement(By.name("homepage")).click();
+        wd.findElement(By.name("homepage")).clear();
+        wd.findElement(By.name("homepage")).sendKeys(ContaktData.getHomepage());
+        wd.findElement(By.name("address2")).click();
+        wd.findElement(By.name("address2")).clear();
+        wd.findElement(By.name("address2")).sendKeys(ContaktData.getHomeaddress());
+        wd.findElement(By.name("phone2")).click();
+        wd.findElement(By.name("phone2")).clear();
+        wd.findElement(By.name("phone2")).sendKeys(ContaktData.getHousenumber());
+        wd.findElement(By.name("notes")).click();
+        wd.findElement(By.name("notes")).clear();
+        wd.findElement(By.name("notes")).sendKeys(ContaktData.getNotes());
+    }
+
+    private void newContaktCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
