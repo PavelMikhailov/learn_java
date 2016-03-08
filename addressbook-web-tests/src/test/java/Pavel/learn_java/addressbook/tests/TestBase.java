1adadbd82efc1f4553f62ea774ddccd65ece1955
PavelMikhailov <mihailov_pd@mail.ru>
2016-03-08 12:40:30 +0300

    Создан EquationTest

--- addressbook-web-tests/src/test/java/Pavel/learn_java/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/Pavel/learn_java/addressbook/tests/TestBase.java
@@ -4,9 +4,6 @@ import Pavel.learn_java.addressbook.appmanager.ApplicationManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-/**
- * Created by Pavel on 05.03.2016.
- */
 public class TestBase {
 
   protected final ApplicationManager app = new ApplicationManager();
